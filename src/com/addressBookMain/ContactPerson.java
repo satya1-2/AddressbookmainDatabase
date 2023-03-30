1edd372dc64ef7d21384ff0531b432b4da037476
satya <satya143@gmail.com>
2023-03-30 17:09:06 +0530

    Added:welcome to addressBook

---
+++ src/com/addressBookMain/ContactPerson.java
@@ -0,0 +1,6 @@
+package com.addressBookMain;
+
+public class ContactPerson {
+    public static void main(String[] args) {
+        System.out.println("welcome to addreassbok main using json,csv,ittrate,dtabase");
+    }}
